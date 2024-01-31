8c27fe8015d00045c3142744682cd427e3d4ba4f
Jan Andrle <andrle.jan@centrum.cz>
2024-01-31 14:41:18 +0100

    :package: dist

--- dist/esm.d.ts
+++ dist/esm.d.ts
@@ -3,6 +3,7 @@ type Action<V>= (this: { value: V, stopPropagation(): void }, ...a: any[])=> typ
 //type SymbolObservable= Symbol;
 type SymbolOnclear= symbol;
 type Actions<V>= Record<string | SymbolOnclear, Action<V>>;
+type OnListenerOptions= Pick<AddEventListenerOptions, "signal"> & { first_time?: boolean };
 interface observable{
 	_: Symbol
 	/**
@@ -40,7 +41,7 @@ interface observable{
 		...params: A[N] extends (...args: infer P)=> any ? P : never
 	): void;
 	clear(...observables: Observable<any, any>[]): void;
-	on<T>(observable: Observable<T, any>, onchange: (a: T)=> void, options?: AddEventListenerOptions): void;
+	on<T>(observable: Observable<T, any>, onchange: (a: T)=> void, options?: OnListenerOptions): void;
 	symbols: {
 		//observable: SymbolObservable;
 		onclear: SymbolOnclear;
@@ -239,6 +240,7 @@ export function customElementRender<
 	P extends any = Record<string, any>
 >(
 	custom_element: EL,
+	target: ShadowRoot | EL,
 	render: (props: P)=> SupportedElement,
 	props?: P | ((...args: any[])=> P)
 ): EL
